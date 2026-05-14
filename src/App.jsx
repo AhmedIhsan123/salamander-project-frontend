674ae97512801893de23d87e3a974863261a7d2a
Elvin Hrytsyuk <183242754+Elvin-code-dev@users.noreply.github.com>
2026-05-14 10:29:11 -0700

    added the video componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
-	return (
-		<>
-			<h1>Hello World!</h1>
-		</>
-	);
-}
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Videos.jsx';
+import Videos from './pages/Home.jsx';
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/videos" element={<Videos />} />
+    </Routes>
+  );
+}
