724f18c1cb83960c0103cd00b46b2246a0db7511
Jake-G123 <jakelgerken@gmail.com>
2026-05-14 11:17:24 -0700

    Added font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
 	return (
-		<div>
+		<div className="">
 			<h1>Salamader Tracker</h1>
 			<p>Pick a video from the Videos page to start analyzing.</p>
-			<h1 className="text-4xl font-bold text-blue-600">Salamander Tracker</h1>
+			<h1 className="text-4xl font-[SchoolBell] text-green-800">Salamander Tracker</h1>
 		</div>
 	);
 }
