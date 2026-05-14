febd38de09f47a05190d0bd522971206ec27a936
Ahmed Ihsan <Ihsan.Ahmed@student.greenriver.edu>
2026-05-14 10:34:12 -0700

    Fixed routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Videos.jsx';
-import Videos from './pages/Home.jsx';
+import { Routes, Route } from "react-router-dom";
+import Videos from "./pages/Videos.jsx";
+import Home from "./pages/Home.jsx";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/videos" element={<Videos />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/videos" element={<Videos />} />
+		</Routes>
+	);
 }
