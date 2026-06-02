430b2d508762ffdae00ef52bd3b15dce6afda22d
Elvin Hrytsyuk <183242754+Elvin-code-dev@users.noreply.github.com>
2026-06-02 10:35:08 -0700

    Swap the mock for real fetch

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getVideos, getThumbnail } from "../mockApi.js";
+import { getVideos, getThumbnail } from "../api.js";
 import { Link } from "react-router-dom";
 
 export default function Videos() {
